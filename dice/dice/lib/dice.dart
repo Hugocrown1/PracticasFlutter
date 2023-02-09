48e878390d38cfa5ccaf4f274560ea2309f29ef9
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-09 11:02:33 -0800

    Ambos dados cambian al ser presionados

--- dice/dice/lib/dice.dart
+++ dice/dice/lib/dice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:math';
 
 class Dice extends StatelessWidget {
   const Dice({super.key});
@@ -33,30 +34,46 @@ class DiceGame extends StatefulWidget {
 }
 
 class _DiceGameState extends State<DiceGame> {
+  int leftdice = 1;
+  int rightdice = 2;
+
+  rollleftdice() {
+    setState(() {
+      leftdice = Random().nextInt(6) + 1;
+    });
+  }
+
+  rollrightdice() {
+    setState(() {
+      rightdice = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextButton(
-              onPressed: () {
-                print("Hola");
-              },
-              child: Image.asset('assets/images/dice1.png')),
-        )),
-        Expanded(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextButton(
-              onPressed: () {
-                print("Chocho");
-              },
-              child: Image.asset('assets/images/dice2.png')),
-        ))
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () {
+                  rollleftdice();
+                },
+                child: Image.asset('assets/images/dice$leftdice.png')),
+          )),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton(
+                onPressed: () {
+                  rollrightdice();
+                },
+                child: Image.asset('assets/images/dice$rightdice.png')),
+          ))
+        ],
+      ),
     );
   }
 }
