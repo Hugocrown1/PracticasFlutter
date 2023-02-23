f7054fa207e7e855296df8fd643d88359ee63683
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-23 12:24:51 -0800

    Se creo el metodo de botones

--- navegacion/navegacion/lib/home_page.dart
+++ navegacion/navegacion/lib/home_page.dart
@@ -16,70 +16,56 @@ class HomePage extends StatelessWidget {
         //Row donde estarán los albumes en fila
         Row(
           children: [
-            //Container donde estarán la portada del album acompañados del nombre y año
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                //Columna para ordenar los componentes de arriba a abajo
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 150,
-                        width: 150,
-                        decoration: BoxDecoration(color: Colors.black),
-                        child: Text("donda"),
-                      ),
-                    ),
-                    Text(
-                      'DONDA\n  2020',
-                      textAlign: TextAlign.justify,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            //Boton del album como tal
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: TextButton(
-                onPressed: () {},
-                child: Container(
-                  //Columna para ordenar los componentes de arriba a abajo
-                  child: Column(
-                    children: [
-                      //Boton con el cover del album, nombre y año de salida
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              image: DecorationImage(
-                                  image: AssetImage('assets/images/tlop.jpg'))),
-                          child: Text(''),
-                        ),
-                      ),
-                      Text(
-                        'The Life Of Pablo',
-                        style: TextStyle(color: Colors.black),
-                        textAlign: TextAlign.justify,
-                      ),
-                      Text(
-                        '2016',
-                        style: TextStyle(color: Colors.black),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            //Botones del album como tal
+            botonAlbum('assets/images/donda.png', 'DONDA', '2021'),
+            botonAlbum('assets/images/tlop.jpg', 'The Life Of Hugo', '2016'),
           ],
         ),
+        Row(
+          children: [
+            botonAlbum('assets/images/mtdf.jpg', 'MBTDF', '2010'),
+            botonAlbum('assets/images/graduation.jpg', 'Graduation', '2007'),
+          ],
+        )
       ],
     )));
+  }
+
+  Padding botonAlbum(cover, nombre, salida) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: TextButton(
+        onPressed: () {},
+        child: Container(
+          //Columna para ordenar los componentes de arriba a abajo
+          child: Column(
+            children: [
+              //Boton con el cover del album, nombre y año de salida
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      image: DecorationImage(image: AssetImage(cover))),
+                  child: Text(''),
+                ),
+              ),
+              Text(
+                nombre,
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.justify,
+              ),
+              Text(
+                salida,
+                style: TextStyle(color: Colors.black),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
