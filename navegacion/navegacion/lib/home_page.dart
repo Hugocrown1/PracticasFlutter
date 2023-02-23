66da2c33ff9786d5061c73f3970bcb7f2986215c
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-23 12:09:35 -0800

    Se creo el boton de album

--- navegacion/navegacion/lib/home_page.dart
+++ navegacion/navegacion/lib/home_page.dart
@@ -10,37 +10,76 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
+        body: SafeArea(
+            child: Column(
       children: [
-        ListTile(
-          title: Text('Ir al login'),
-          subtitle: Text('Te manda al login'),
-          trailing: Icon(Icons.arrow_forward),
-          leading: CircleAvatar(
-            child: Text('🏠'),
-          ),
-          onTap: () {
-            final ruta = MaterialPageRoute(
-              builder: (_) => const LoginPage(),
-            );
-            Navigator.push(context, ruta);
-          },
+        //Row donde estarán los albumes en fila
+        Row(
+          children: [
+            //Container donde estarán la portada del album acompañados del nombre y año
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                //Columna para ordenar los componentes de arriba a abajo
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(color: Colors.black),
+                        child: Text("donda"),
+                      ),
+                    ),
+                    Text(
+                      'DONDA\n  2020',
+                      textAlign: TextAlign.justify,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            //Boton del album como tal
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Container(
+                  //Columna para ordenar los componentes de arriba a abajo
+                  child: Column(
+                    children: [
+                      //Boton con el cover del album, nombre y año de salida
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/tlop.jpg'))),
+                          child: Text(''),
+                        ),
+                      ),
+                      Text(
+                        'The Life Of Pablo',
+                        style: TextStyle(color: Colors.black),
+                        textAlign: TextAlign.justify,
+                      ),
+                      Text(
+                        '2016',
+                        style: TextStyle(color: Colors.black),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        ListTile(
-          title: Text('Ir al help screen'),
-          subtitle: Text('Te manda a la pantalla de ayuda'),
-          trailing: Icon(Icons.arrow_forward),
-          leading: CircleAvatar(
-            child: Text('🥶'),
-          ),
-          onTap: () {
-            final ruta = MaterialPageRoute(
-              builder: (_) => const HelpPage(),
-            );
-            Navigator.push(context, ruta);
-          },
-        )
       ],
-    ));
+    )));
   }
 }
