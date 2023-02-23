f89eb17a529334294367b86a23375c8358d9bf99
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-23 12:52:17 -0800

    Se creó la DondaScreen

--- navegacion/navegacion/lib/home_page.dart
+++ navegacion/navegacion/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:navegacion/screen/donda_screen.dart';
 import 'package:navegacion/screen/help_page.dart';
 import 'package:navegacion/screen/login_page.dart';
 
@@ -14,6 +15,7 @@ class HomePage extends StatelessWidget {
             child: Column(
       children: [
         //Row donde estarán los albumes en fila
+        //Primera fila
         Row(
           children: [
             //Botones del album como tal
@@ -21,6 +23,7 @@ class HomePage extends StatelessWidget {
             botonAlbum('assets/images/tlop.jpg', 'The Life Of Hugo', '2016'),
           ],
         ),
+        //Segunda fila
         Row(
           children: [
             botonAlbum('assets/images/mtdf.jpg', 'MBTDF', '2010'),
@@ -47,6 +50,7 @@ class HomePage extends StatelessWidget {
                   height: 150,
                   width: 150,
                   decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
                       color: Colors.black,
                       image: DecorationImage(image: AssetImage(cover))),
                   child: Text(''),
---
+++ navegacion/navegacion/lib/screen/donda_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DondaScreen extends StatelessWidget {
+  const DondaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
