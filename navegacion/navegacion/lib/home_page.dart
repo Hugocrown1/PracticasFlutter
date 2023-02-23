77b3415fcb0d84267d8fd8c0cb8ac29671088e39
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-23 13:47:46 -0800

    Se avanzó en la pantalla de album

--- navegacion/navegacion/lib/home_page.dart
+++ navegacion/navegacion/lib/home_page.dart
@@ -19,8 +19,30 @@ class HomePage extends StatelessWidget {
         Row(
           children: [
             //Botones del album como tal
-            botonAlbum('assets/images/donda.png', 'DONDA', '2021'),
-            botonAlbum('assets/images/tlop.jpg', 'The Life Of Hugo', '2016'),
+
+            //Donda Button
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                  onPressed: () {
+                    final ruta = MaterialPageRoute(
+                      builder: (_) => const DondaScreen(),
+                    );
+                    Navigator.push(context, ruta);
+                  },
+                  child:
+                      botonAlbum('assets/images/donda.png', 'DONDA', '2021')),
+            ),
+
+            //TLOP Button
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton(
+                onPressed: () {},
+                child: botonAlbum(
+                    'assets/images/tlop.jpg', 'The Life Of Hugo', '2016'),
+              ),
+            ),
           ],
         ),
         //Segunda fila
@@ -34,41 +56,35 @@ class HomePage extends StatelessWidget {
     )));
   }
 
-  Padding botonAlbum(cover, nombre, salida) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: TextButton(
-        onPressed: () {},
-        child: Container(
-          //Columna para ordenar los componentes de arriba a abajo
-          child: Column(
-            children: [
-              //Boton con el cover del album, nombre y año de salida
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 150,
-                  width: 150,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black,
-                      image: DecorationImage(image: AssetImage(cover))),
-                  child: Text(''),
-                ),
-              ),
-              Text(
-                nombre,
-                style: TextStyle(color: Colors.black),
-                textAlign: TextAlign.justify,
-              ),
-              Text(
-                salida,
-                style: TextStyle(color: Colors.black),
-                textAlign: TextAlign.justify,
-              ),
-            ],
+  Container botonAlbum(cover, nombre, salida) {
+    return Container(
+      //Columna para ordenar los componentes de arriba a abajo
+      child: Column(
+        children: [
+          //Boton con el cover del album, nombre y año de salida
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                  image: DecorationImage(image: AssetImage(cover))),
+              child: Text(''),
+            ),
           ),
-        ),
+          Text(
+            nombre,
+            style: TextStyle(color: Colors.black),
+            textAlign: TextAlign.justify,
+          ),
+          Text(
+            salida,
+            style: TextStyle(color: Colors.black),
+            textAlign: TextAlign.justify,
+          ),
+        ],
       ),
     );
   }
--- navegacion/navegacion/lib/screen/donda_screen.dart
+++ navegacion/navegacion/lib/screen/donda_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -6,6 +7,66 @@ class DondaScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child:
+              //Columna que contiene todos los componentes
+              Column(
+        children: [
+          //Fila que contendrá cover del album, boton de play, boton de like y boton de opciones
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //Album cover
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Text(''),
+                ),
+              ),
+
+              //Columna que contiene la informacion del album
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'DONDA',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      style: TextStyle(fontSize: 15),
+                      'Kanye West · 2021 · 10 canciones',
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(12, 5, 5, 12),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  child: Icon(
+                    Icons.play_arrow,
+                    size: 25,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
