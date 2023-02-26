e3bf378cf6d58d4631cb5d50d588c20e35d29d91
Hugocrown1 <hugo.corona.r@gmail.com>
2023-02-25 20:24:43 -0800

    Finalizada la pantalla de album

--- navegacion/navegacion/lib/screen/donda_screen.dart
+++ navegacion/navegacion/lib/screen/donda_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:navegacion/home_page.dart';
 
 class DondaScreen extends StatelessWidget {
   const DondaScreen({super.key});
@@ -18,16 +19,25 @@ class DondaScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Icon(
-                  Icons.arrow_back,
-                  size: 35,
+              TextButton(
+                onPressed: () {
+                  final ruta = MaterialPageRoute(
+                    builder: (_) => const HomePage(),
+                  );
+                  Navigator.push(context, ruta);
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(1.0),
+                  child: Icon(
+                    Icons.arrow_back,
+                    size: 35,
+                    color: Colors.amber,
+                  ),
                 ),
               ),
               //Album cover
               Padding(
-                padding: const EdgeInsets.fromLTRB(60, 8, 8, 3),
+                padding: const EdgeInsets.fromLTRB(48, 8, 8, 3),
                 child: Container(
                   height: 180,
                   width: 180,
@@ -48,7 +58,7 @@ class DondaScreen extends StatelessWidget {
               children: [
                 Text(
                   'DONDA',
-                  style: TextStyle(fontSize: 25),
+                  style: TextStyle(fontSize: 25, fontFamily: 'FranklinMedium'),
                 ),
                 Text(
                   style: TextStyle(fontSize: 15),
@@ -80,24 +90,38 @@ class DondaScreen extends StatelessWidget {
           ),
           SizedBox(
             width: 450,
-            height: 100,
+            height: 320,
             child: ListView(
               children: [
-                ListTile(
-                  title: Text(
-                    'DONDA',
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                  subtitle: Text('Kanye West'),
-                  trailing: Icon(Icons.audiotrack),
-                )
+                cancionLista('Donda Chant'),
+                cancionLista('Off The Grid'),
+                cancionLista('Hurricane'),
+                cancionLista('Praise God'),
+                cancionLista('No Child Left Behind'),
+                cancionLista('Come To Life'),
+                cancionLista('Lord I Need You'),
+                cancionLista('Jesus God'),
+                cancionLista('New Again'),
+                cancionLista('Tell the Vision'),
               ],
             ),
           )
         ],
       )),
+    );
+  }
+
+  ListTile cancionLista(nombreCancion) {
+    return ListTile(
+      title: Text(
+        nombreCancion,
+        style: TextStyle(
+            fontSize: 18,
+            fontFamily: 'FranklinMedium',
+            fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text('Kanye West'),
+      trailing: Icon(Icons.audiotrack),
     );
   }
 }
